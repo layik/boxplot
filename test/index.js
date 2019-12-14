caec384ccf5e3e6ee7574e1f229de93401ef25cc
Layik Hama <layik.hama@gmail.com>
2019-12-14 23:34:10 +0000

    test and coverage

--- test/index.js
+++ test/index.js
@@ -1,12 +1,27 @@
 
+import 'jsdom-global/register'; 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import { mount } from 'enzyme';
 
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+import Boxplot from '../src/boxplot';
+
+var expect = require('chai').expect;
+
+it('Mount', () => {
+  let m = mount(<Boxplot />);
+  // console.log(m.props());
+  // null is returned without data
+  expect(m.contains(<div />)).equal(false);
+  expect(m.contains('div')).equal(false);
+  m = mount(<Boxplot data={[1,3,4,80,20,2]} />);
+  expect(m.find('div')).to.have.lengthOf(2);
+  m = mount(<Boxplot data={['a','lla','blah','foo']} />);
+  //not numeric
+  expect(m.contains(<div />)).equal(false);
+  // large digits
+  m = mount(<Boxplot data={[1200,30888,4,80,20,2]} />);
+  expect(m.find('div')).to.have.lengthOf(2);
+})
