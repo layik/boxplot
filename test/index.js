21a68c352f67a207c10e14e2ae79483943e22408
Layik Hama <layik.hama@gmail.com>
2019-12-14 18:52:45 +0000

    tests init

---
+++ test/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
